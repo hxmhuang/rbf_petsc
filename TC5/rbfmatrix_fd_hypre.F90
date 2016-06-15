aef728cdb715d332588ffeaa4988db7fe453e6f6
HuangXiaomeng <hxm@HuangXiaomengdeMacBook-Pro.local>
2016-06-16 01:31:49 +0800

    Obtained DPx,DPy,DPz and L.

--- TC5/rbfmatrix_fd_hypre.F90
+++ TC5/rbfmatrix_fd_hypre.F90
@@ -15,6 +15,7 @@ subroutine rbfmatrix_fd_hypre(atm,ep,fdsize,order,dim,DPx,DPy,DPz,L,ierr)
     type(Matrix)        		  :: weights,weightsDx,weightsDy,weightsDz,weightsL
     type(Matrix)        		  :: ind_i,ind_j 
     type(Matrix)        		  :: hypre_result
+    type(Matrix)        		  :: m_0,m_0to2,m_0toN 
 	integer 					  :: N,j,myrank
 	integer 					  :: ista,iend
     
@@ -43,12 +44,15 @@ subroutine rbfmatrix_fd_hypre(atm,ep,fdsize,order,dim,DPx,DPy,DPz,L,ierr)
 	weightsDy=dm_zeros(0,1)
 	weightsDz=dm_zeros(0,1)
 	weightsL=dm_zeros(0,1)
-!	!do j=0,N-1
-    do j=0,8
+
+	m_0to2=dm_m2n(0,2)
+	m_0toN=dm_m2n(0,fdsize-1)
+	m_0	  =dm_zeros(1,1)	
+ 	do j=0,N-1
 !   !do j=0,0
  		if(myrank==0) print *,">Rbfmatrix_df_hypre: current", j, " step"
   		imat	= dm_trans(dm_getrow(idx,j))
-  		ximat  	= dm_submatrix(x,imat,dm_m2n(0,2)) 
+  		ximat  	= dm_submatrix(x,imat,m_0to2) 
     	
     	ind_i = ind_i .vj. dm_constants(fdsize,1,j)	
     	ind_j = ind_j .vj. imat 
@@ -64,21 +68,19 @@ subroutine rbfmatrix_fd_hypre(atm,ep,fdsize,order,dim,DPx,DPy,DPz,L,ierr)
  		A= A .vj. (dm_ones(1,A%ncol))
      	call dm_setvalue(A,A%nrow-1,A%ncol-1,0,ierr)	
     	B=(-1.0)*ximat * dm_trans(dm_getrow(atm%pts%p_u,j)) .em. drbf_rd2v
-    	B=B .vj. (dm_zeros(1,1))
+    	B=B .vj. m_0
     	weights= A .inv. B
-    	!call dm_view(weights,ierr)
-    	weightsDx= weightsDx .vj. (dm_submatrix(weights, dm_m2n(0,fdsize-1), dm_zeros(1,1))) 
- 		!call dm_view(weightsDx,ierr)
+    	weightsDx= weightsDx .vj. (dm_submatrix(weights, m_0toN, m_0)) 
     	
      	B=(-1.0)*ximat * dm_trans(dm_getrow(atm%pts%p_v,j)) .em. drbf_rd2v
-     	B=B .vj. (dm_zeros(1,1))
+     	B=B .vj. m_0 
     	weights= A .inv. B
-    	weightsDy= weightsDy .vj. (dm_submatrix(weights, dm_m2n(0,fdsize-1), dm_zeros(1,1))) 
+    	weightsDy= weightsDy .vj. (dm_submatrix(weights, m_0toN, m_0)) 
     		
     	B=(-1.0)*ximat * dm_trans(dm_getrow(atm%pts%p_w,j)) .em. drbf_rd2v
-    	B=B .vj. (dm_zeros(1,1))
+    	B=B .vj. m_0 
     	weights= A .inv. B
-    	weightsDz= weightsDz .vj. (dm_submatrix(weights, dm_m2n(0,fdsize-1), dm_zeros(1,1))) 
+    	weightsDz= weightsDz .vj. (dm_submatrix(weights, m_0toN, m_0)) 
     	
     	!There is a bug to put a MAT_XTYPE_IMPLICIT matrix into a function directly.
     	!hypre_result=hypre(ep**2 * rd2v,dim,order)
@@ -86,12 +88,21 @@ subroutine rbfmatrix_fd_hypre(atm,ep,fdsize,order,dim,DPx,DPy,DPz,L,ierr)
      	hypre_result=hypre(ep2r2,dim,order)
     	
     	B=ep**(2*order) * hypre_result .em. (rbf_guassian(ep,rd2v))
-    	B=B .vj. (dm_zeros(1,1))
+    	B=B .vj. m_0 
     	weights= A .inv. B
-    	weightsL= weightsL .vj. (dm_submatrix(weights, dm_m2n(0,fdsize-1), dm_zeros(1,1))) 
+    	weightsL= weightsL .vj. (dm_submatrix(weights, m_0toN, m_0)) 
 
     enddo
-	!call dm_view(weightsL,ierr)
+	
+	DPx=dm_sparse(ind_i,ind_j,weightsDx,N,N)
+	DPy=dm_sparse(ind_i,ind_j,weightsDy,N,N)
+	DPz=dm_sparse(ind_i,ind_j,weightsDz,N,N)
+	L  =dm_sparse(ind_i,ind_j,weightsL,N,N)
+
+	!call dm_view(DPx,ierr)
+	!call dm_view(DPy,ierr)
+	!call dm_view(DPz,ierr)
+	!call dm_view(L,ierr)
     call dm_destroy(A,ierr) 
     call dm_destroy(B,ierr) 
     call dm_destroy(idx,ierr) 
@@ -110,6 +121,9 @@ subroutine rbfmatrix_fd_hypre(atm,ep,fdsize,order,dim,DPx,DPy,DPz,L,ierr)
     call dm_destroy(drbf_rd2v,ierr) 
     call dm_destroy(ep2r2,ierr) 
     call dm_destroy(hypre_result,ierr) 
+    call dm_destroy(m_0,ierr) 
+    call dm_destroy(m_0to2,ierr) 
+    call dm_destroy(m_0toN,ierr) 
 end subroutine
 
 
--- TC5/test_case_5_cart_rk4_fd.F90
+++ TC5/test_case_5_cart_rk4_fd.F90
@@ -15,5 +15,9 @@ subroutine test_case_5_cart_rk4_fd(nfile,ep,fdsize,order,dim,gamma,dt,tend,ierr)
 	
 	call rbfmatrix_fd_hypre(atm,ep,fdsize,order,dim,DPx,DPy,Dpz,L,ierr)	
 		
+    call dm_destroy(DPx,ierr)
+    call dm_destroy(DPy,ierr)
+    call dm_destroy(DPz,ierr)
+    call dm_destroy(L,ierr)
     call atm_destroy(atm,ierr)
 end subroutine
